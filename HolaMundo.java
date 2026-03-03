5bf822d7cb1c32828e6803942e24e57dd1398aeb
gusvicmtz-boop <gus.vic.mtz@gmail.com>
2026-03-02 23:16:33 -0600

    first commit

---
+++ HolaMundo.java
@@ -0,0 +1,18 @@
+// para un comentario de una sola linea
+//Java es un lenguaje de programcion multiparadigma significa que se adpata a las necesidades del modelo o arquitectura de la programacion.
+
+//Java fue originalmente creado para programar electrodomesticos, este lenguaje ultiliza una maquina virtual de Java.
+
+//Esa maquina se encarga de transformar el codigo de la maquina 01 en las instrucciones definidas para el programador y viceversa
+
+
+class Holamundo{
+    //Este es el cuerpo de la clase
+    //Aqui se define las variables globales
+
+    //Aqui definimos los comportamientos
+
+    public static void main(String[] args){
+        System.out.println("Hola mundo");
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# C-Users-Alumno-Desktop-4IV9_Apppat_Apmat_Victoria-Martinez-Gustavo
