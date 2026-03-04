63da364eeb45a75762081d3ed223d32ed736bb56
gusvicmtz-boop <gus.vic.mtz@gmail.com>
2026-03-04 15:43:00 -0600

    EntradaTexto

---
+++ EntradaTexto.java
@@ -0,0 +1,19 @@
+/* Vamos a crear una instancia de un objeto para la entrada de datos estamdar y la salida de datos */
+//Liberar scanner
+import java.util.Scanner;
+class EntradaTexto{
+    //Metodo principal
+    public static void main(String[] args){
+        //Instamos el objeto
+        Scanner entrada = new Scanner(System.in);
+        //Las variables locales
+        String nombre;
+        int edad;
+        //Entrada
+        System.out.println("Escribe tu nombre: ");
+        nombre = System.console().readLine();
+        System.out.println("Hola " + nombre + "Bienvenido a programar muy feo");
+        System.out.println("Escribe tu edad: ");
+        edad = entrada.nextInt();
+    } 
+}
