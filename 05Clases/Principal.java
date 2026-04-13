930e93eff30ac51c9445a973d2175e22d8a2af5a
Martinez-progra <gvictoriam2400@alumno.ipn.mx>
2026-04-13 17:42:36 -0600

    Ejercicios segundo parcial

---
+++ 05Clases/Principal.java
@@ -0,0 +1,11 @@
+//ya vimos el concepto de clase
+
+class Principal {
+    //metodo principal
+    public static void main(String[] args) {
+        //un ejercicio de figuritas
+    ejercicios ejecucion = new ejercicios();
+    ejecucion.menu();
+    }
+    
+}
