84fae1361a6ba7ec559cd2ebc68e4bcb8e261a4b
gusvicmtz-boop <gus.vic.mtz@gmail.com>
2026-04-19 14:18:28 -0600

    Semana del 12/04

--- 05Clases/ejercicios.java
+++ 05Clases/ejercicios.java
@@ -1,38 +1,48 @@
 import java.util.Scanner;
 
-public class ejercicios{
-    //Vamos a hacer una calculadora
-    //La calculadora debera tener un menu
+public class ejercicios {
+    
+    //vamos hacer una calculadora rapida
 
     Scanner entrada = new Scanner(System.in);
 
+
+
+    //la calculadora debe de tener un menu
     public void menu(){
-        System.out.println("Bienvenido a la caluladora, selecciona la opcion que escogera");
-        System.out.println("1. Sumar");
-        System.out.println("2. Restar");
-        System.out.println("3. Multiplicar");
-        System.out.println("4. Salir");
+        //las instrucciones
+        System.out.println("Bienvenido a la calculadora");
+        System.out.println("Elige la opcion deseada");
+        System.out.println("1.- Sumar");
+        System.out.println("2.- Restar");
+        System.out.println("3.- Multiplicar");
+        System.out.println("4.- Salir");
 
         int opcion=0;
 
         opcion = entrada.nextInt();
+        
         switch (opcion) {
             case 1:
-                suma();
+                suma();             
                 break;
+        
             default:
                 break;
         }
-        }
-        public void suma(){
-            double num1 = 0.00, num2= 0.00;
-            System.out.println("De el valor 1");
-            num1 = entrada.nextDouble();
-            System.out.println("DE EL VALOR 2");
-            num2 = entrada.nextDouble();
-            resultadO = num1 + num2;
 
-            System.out.println("Resultado: " + resultadO);
-        }
+
     }
 
+    public void suma(){
+        double num1 = 0.00, num2=0.00, resultado=0.00;
+
+        System.out.println("Ingresa el numero que deseas sumar:");
+        num1 = entrada.nextDouble();
+        System.out.println("Ingresa el numero que deseas sumar:");
+        num2 = entrada.nextDouble();
+        resultado = num1 + num2;
+        System.out.println("La suma es: " + resultado);
+        
+    }
+}
